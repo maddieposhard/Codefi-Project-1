c1abac1a4a3b2a586d7cda6f4908411da1d34223
maddieposhard <poshardmadeline@gmail.com>
2025-02-21 15:41:39 -0600

    favorites and delete button display on favorites page

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -3,7 +3,26 @@ import { favorites } from "../data/data.js";
 const favoritesText = document.querySelector('.favorites-text')
 const favoritesList = document.querySelector('.favorites-list')
 
-function displayFavorites () {
+// export function displayFavorites () {
+//     let favorites = JSON.parse(localStorage.getItem('favorites'));
+//     if (favorites.length === 0) {
+//         favoritesList.innerHTML = '<p>No favorites yet!</p>';
+//     } else {
+//         favorites.forEach (affirmation => {
+//             let favoritesListItem = document.createElement('li');
+//             favoritesListItem.textContent = affirmation;
+//             favoritesList.appendChild(favoritesListItem);
+//         })
+//     }
+// }
+
+function deleteFavorite (index) {
+    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    favorites.splice(index, 1);
+    localStorage.setItem('favorites', JSON.stringify(favoriteAffirmations));
+}
+
+function reloadFavorites () {
     let favorites = JSON.parse(localStorage.getItem('favorites'));
     if (favorites.length === 0) {
         favoritesList.innerHTML = '<p>No favorites yet!</p>';
@@ -11,11 +30,15 @@ function displayFavorites () {
         favorites.forEach (affirmation => {
             let favoritesListItem = document.createElement('li');
             favoritesListItem.textContent = affirmation;
+            let deleteBtn = document.createElement('button');
+            deleteBtn.textContent = '&times;';
+            deleteBtn.onclick = () => deleteFavorite(index);
+
+            favoritesListItem.appendChild(deleteBtn);
             favoritesList.appendChild(favoritesListItem);
         })
     }
 }
 
-displayFavorites();
+reloadFavorites();
 
-console.log(favorites)
