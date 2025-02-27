f5d2103ba75d82d9e65454f3d5bcfdb3b35bcb96
maddieposhard <poshardmadeline@gmail.com>
2025-02-27 10:51:02 -0600

    Made all button modals functional

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import { affirmations, favorites } from "../data/data.js";
 
+const modalContainer = document.querySelector('.modal-container');
 const affirmationContainer = document.querySelector(".affirmation-container");
 const affirmationText = document.querySelector(".affirmation-text");
 const newBtn = document.querySelector(".new-btn");
@@ -7,9 +8,8 @@ const shareBtn = document.querySelector(".share-btn");
 let submitBtn = document.querySelector(".submit-btn");
 const submitForm = document.querySelector(".submit-form");
 const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
 const saveBtn = document.querySelector(".save-btn");
-const data = JSON.parse(localStorage.getItem("affirmations"));
+export const data = JSON.parse(localStorage.getItem("affirmations"));
 
 // first load display(affirmations) on formBtn display(data) => favorites => home = first load
 
@@ -25,7 +25,7 @@ function displayAffirmation(affirmationsList) {
   let i = Math.floor(Math.random() * affirmationsList.length);
   const affirmation = affirmationsList[i];
   affirmationText.textContent = affirmation;
-  console.log(affirmationsList);
+//   console.log(affirmationsList);
 }
 
 newBtn.addEventListener("click", () => {
@@ -45,7 +45,7 @@ shareBtn.addEventListener("click", function () {
 });
 
 //I would like the form content to reset after closing and reopening the modal
-//I also want it to show if an affirmation is the same as one already in the array
+//I want it to show if an affirmation is the same as one already in the array
 
 function addAffirmation(e) {
   e.preventDefault();
@@ -61,45 +61,34 @@ function addAffirmation(e) {
   } else {
     alert("Please enter your affirmation!");
   }
-  submitBtn = document.querySelector(".submit-btn");
+  console.log(data);
 }
 
 
 submitBtn.addEventListener("click", function () {
   modal.removeAttribute("style");
-
-    console.log("clicked")
   modal.removeAttribute("hidden")
   modal.innerHTML = displayModal()
   const modalForm = document.querySelector(".modal-form")
   modalForm.removeAttribute('hidden')
 
+  const close = document.querySelector(".close");
+  close.addEventListener("click", function () {
+    console.log("close");
+    modalContainer.style.display = 'none';
+    affirmationContainer.classList.remove("blur");
+  });
+
   affirmationContainer.classList.add("blur");
-  submitBtn = document.querySelector(".form-btn");
-  submitBtn.addEventListener("click", function (e) {
+  let formBtn = document.querySelector(".form-btn");
+  formBtn.addEventListener("click", function (e) {
   addAffirmation(e);
-  
-  modal.style.display += "none";
+  modalContainer.style.display += "none";
   affirmationContainer.classList.remove("blur");
   const data = JSON.parse(localStorage.getItem("affirmations"));
   displayAffirmation(data);
   });
 });
-
-// close.addEventListener("click", function () {
-//     console.log("clicked");
-    
-//     modal.setAttribute("style", "display: none;")
-//     affirmationContainer.classList.remove("blur");
-//   });
-
-
-  
-  
-  
-
-
-
 
 
 
@@ -109,34 +98,47 @@ function addFavorite() {
   if (!favorites.includes(currentAffirmation)) {
     favorites.push(currentAffirmation);
     localStorage.setItem("favorites", JSON.stringify(favorites));
-    console.log("Saved to favorites:", currentAffirmation);
+    modal.innerHTML += displayModal();
+    modal.removeAttribute("hidden");
+    modal.removeAttribute("style");
+    const savedModal = document.querySelector(".savedModal");
+    savedModal.removeAttribute('hidden');
+    const close = document.querySelector('.close');
+    close.addEventListener("click", function () {
+        console.log("close");
+        modalContainer.style.display = 'none';
+        affirmationContainer.classList.remove("blur");
+      });
     console.log("Favorite Affirmations:", favorites);
   } else {
-
-    // change the form to hidden, remove hidden attribute from a ptag
-    modal.innerHTML += displayModal()
-    modal.removeAttribute("hidden")
-    modal.removeAttribute("style")
+    modal.innerHTML += displayModal();
+    modal.removeAttribute("hidden");
+    modal.removeAttribute("style");
     const favoritesModal = document.querySelector(".favoritesModal")
     favoritesModal.removeAttribute('hidden')
+    const close = document.querySelector(".close");
+    close.addEventListener("click", function () {
+      console.log("close");
+      modalContainer.style.display = 'none';
+      affirmationContainer.classList.remove("blur");
+    });
   }
 }
 
 saveBtn.addEventListener("click", () => addFavorite());
 
 function displayModal() {
+  
     const modalHTML =`
-   <div class="modal-content">
-           <span class="close">&times;</span>
-           <form class="modal-form" class="submit-form" hidden >
-               <input type="text" class="affirmation-input" placeholder="Type here!">
-               <button class="form-btn" type="button">Submit</button>
-            </form>
-       <p class="favoritesModal" hidden>Affirmation is already in favorites</p>
-       </div>`
-    console.log(modalHTML)
-    return modalHTML
-}
-
-
-
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <form class="modal-form" class="submit-form" hidden>
+            <input type="text" class="affirmation-input" placeholder="Type here!">
+            <button class="form-btn" type="button">Submit</button>
+        </form>
+        <p class="favoritesModal" hidden>Affirmation is already in favorites</p>
+        <p class="savedModal" hidden>Saved to favorites</p>
+    </div>`;
+    modalContainer.innerHTML = modalHTML;
+    modalContainer.removeAttribute('style')
+    return modalHTML}
